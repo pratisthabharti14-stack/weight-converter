e20bae867382d1a6f15203afd4dd2b01c7d8632a
pratisthabharti14-stack <pratisthabharti14@gmail.com>
2026-07-19 08:08:47 +0000

    fix calculation  formula logic

--- weight.py
+++ weight.py
@@ -2,9 +2,9 @@ weight = int(input("Weight: "))
 unit = input( "(K)g or (L)bs:  ")
 
 if unit.upper() == "K":
-      converted = weight / 0.45
+      converted = weight * 2.20462
       print( "weight in Lbs: " + str(converted))
 
 else:
-      converted = weight * 0.45
+      converted = weight / 2.20462
       print("weight in Kgs: " + str( converted)) 
